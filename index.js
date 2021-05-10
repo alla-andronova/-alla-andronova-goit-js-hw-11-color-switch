7568756c41308aa05b96cc2f2fb36842c2cb0950
alla-andronova <allaitcreative@gmail.com>
2021-05-10 17:32:39 +0300

    added color switcher on the body

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const resStartBtn = document.querySelector('.class');
+const refs = {
+  startBtn: document.querySelector('[data-action="start"]'),
+  stoptBtn: document.querySelector('[data-action="stop"]'),
+  body: document.querySelector('body'),
+};
 
-resStartBtn.addEventListener('click', () => console.log('hello'));
+const onstartBtnClick = () => {
+  refs.body.style.backgroundColor = colors[randomIntegerFromInterval(2, 2)];
+};
+
+refs.startBtn.addEventListener('click', onstartBtnClick);
